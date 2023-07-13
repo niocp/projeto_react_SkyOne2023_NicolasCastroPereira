439708b47b8a2e9ee84c89097ec547d00610f22e
Nicolas <luluchvisk@gmail.com>
2023-07-13 20:59:18 -0300

    Etapa 10 concluída!

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -124,6 +124,23 @@ class Game extends React.Component {
 
     }
 
+    const moves = history.map((step, move) => {
+
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+
+      return (
+
+        <li>
+
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+
+        </li>
+
+      );
+    });
+
     return (
 
       <div className="game">
@@ -142,7 +159,7 @@ class Game extends React.Component {
         <div className="game-info">
 
           <div> {status} </div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
 
         </div>
       </div>
