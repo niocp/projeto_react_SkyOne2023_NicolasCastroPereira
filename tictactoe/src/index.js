1925dc2056061c384064d1ace74a5069fe7c5b5c
Nicolas <luluchvisk@gmail.com>
2023-07-13 20:10:44 -0300

    Etapa 8 concluída!

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -32,13 +32,22 @@ class Board extends React.Component {
   handleClick(i) {
 
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
+
+    if (calculateWinner(squares) || squares[i]) {
+
+      return;
+
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+
     this.setState({
 
       squares: squares,
       xIsNext: !this.state.xIsNext,
 
     });
+
   }
 
   renderSquare(i) {
@@ -52,7 +61,18 @@ class Board extends React.Component {
 
   render() {
 
-    const status = 'Next player: X' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+
+    if (winner) {
+
+      status = 'Winner: ' + winner;
+
+    } else {
+
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+
+    }
 
     return (
 
@@ -119,3 +139,33 @@ ReactDOM.render(
   document.getElementById('root')
 
 );
+
+function calculateWinner(squares) {
+
+  const lines = [
+
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+
+    const [a, b, c] = lines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+
+      return squares[a];
+
+    }
+  }
+
+  return null;
+
+}
